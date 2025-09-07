6f1657d32969bbbdad155fc998b5805157af12f2
Fahim2023 <fahimshahrier2023@gmail.com>
2025-09-07 21:19:07 +0600

    added a file where by default all card's will be shown

--- script.js
+++ script.js
@@ -64,3 +64,37 @@ const displayPlants = (plants) => {
     `;
   });
 };
+
+const showAllPlants = () => {
+  fetch("https://openapi.programming-hero.com/api/plants")
+    .then((res) => res.json())
+    .then((data) => {
+      displayAllPlants(data.plants);
+    });
+};
+
+const displayAllPlants = (plant) => {
+  // console.log(plant);
+  plant.forEach((plnt) => {
+    plantContainer.innerHTML += `
+     
+ <div class="card bg-base-100 w-96 shadow-sm h-[500px]">
+  <figure>
+    <img
+      src="${plnt.image}" />
+  </figure>
+  <div class="card-body">
+    <h2 class="card-title font-bold">${plnt.name}</h2>
+    <p class="font-semibold">${plnt.description}</p>
+   <div class="flex justify-between items-center">
+    <div class="card-actions justify-start">
+      <button class="btn btn-outline btn-success rounded-xl">${plnt.category}</button>
+  </div>
+  <div class="text-green-700 font-bold text-lg"><p>৳<span>${plnt.price}</span></p></div>
+   </div>
+  <button class="btn bg-green-600 text-white w-full rounded-3xl mt-6">Add to Cart</button>
+</div>
+    `;
+  });
+};
+showAllPlants();
