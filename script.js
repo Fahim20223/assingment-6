b226fb5f6cea21ccb93ded47add408bb1f5ec57d
Fahim2023 <fahimshahrier2023@gmail.com>
2025-09-07 18:57:26 +0600

    added cards in file

--- script.js
+++ script.js
@@ -1,35 +1,53 @@
 const categoryContainer = document.getElementById("category-container");
+const plantContainer = document.getElementById("plant-container");
 
+// Load categories
 fetch("https://openapi.programming-hero.com/api/categories")
   .then((res) => res.json())
   .then((data) => {
-    console.log(data.categories);
-    const categories = data.categories;
-    displayCategories(categories);
+    displayCategories(data.categories);
   });
 
 const displayCategories = (categories) => {
   categories.forEach((cat) => {
-    console.log(cat.category_name);
     categoryContainer.innerHTML += `
       <li id="${cat.id}"
-              class="hover:bg-green-600 hover:text-white p-2 rounded-lg cursor-pointer"
-            >
-              ${cat.category_name}
-            </li>
-     
-     `;
+          class="hover:bg-green-600 hover:text-white p-2 rounded-lg cursor-pointer">
+        ${cat.category_name}
+      </li>
+    `;
   });
+
+  // Handle category click
   categoryContainer.addEventListener("click", (e) => {
     const allLi = document.querySelectorAll("li");
-    allLi.forEach((li) => {
-      li.classList.remove("bg-green-600");
-    });
+    allLi.forEach((li) => li.classList.remove("bg-green-600"));
 
-    console.log(e.target.localName);
-    if ((e.target.localName = "li")) {
+    if (e.target.localName == "li") {
       e.target.classList.add("bg-green-600");
-      // e.target.classList.add("text-white");
+      loadAllPlants(e.target.id);
     }
+  });
+};
+
+// Load plants by category
+const loadAllPlants = (id) => {
+  fetch(`https://openapi.programming-hero.com/api/category/${id}`)
+    .then((res) => res.json())
+    .then((data) => {
+      displayPlants(data.plants);
+    });
+};
+
+// Show plants
+const displayPlants = (plants) => {
+  plantContainer.innerHTML = ""; // clear previous
+  plants.forEach((plt) => {
+    plantContainer.innerHTML += `
+      <div >
+        <img src="${plt.image}" />
+        <h3 class="mt-2 font-bold">${plt.name}</h3>
+      </div>
+    `;
   });
 };
