6575a275378dacc79c91d3e82dea3ff83a49e2e7
Fahim2023 <fahimshahrier2023@gmail.com>
2025-09-08 17:16:49 +0600

    added Add to cart aside

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 const categoryContainer = document.getElementById("category-container");
 const plantContainer = document.getElementById("plant-container");
-
-const cartContainer = document.getElementById("cart-container");
-// const cartContainer = getElementById("cart-bg");
+const cartContainer = document.getElementById("cart-bg");
+let cart = [];
 
 // Load categories
 fetch("https://openapi.programming-hero.com/api/categories")
@@ -53,7 +52,7 @@ const displayPlants = (plants) => {
     <img
       src="${plt.image}" />
   </figure>
-  <div class="card-body">
+  <div id="${plt.id}" class="card-body">
     <h2 class="card-title font-bold">${plt.name}</h2>
     <p class="font-semibold">${plt.description}</p>
    <div class="flex justify-between items-center">
@@ -62,7 +61,7 @@ const displayPlants = (plants) => {
   </div>
   <div class="text-green-700 font-bold text-lg"><p>৳<span>${plt.price}</span></p></div>
    </div>
-  <button id="cart-container" class="btn bg-green-600 text-white w-full rounded-3xl mt-6">Add to Cart</button>
+  <button class="btn bg-green-600 text-white w-full rounded-3xl mt-6">Add to Cart</button>
 </div>
     `;
   });
@@ -79,7 +78,7 @@ const showAllPlants = () => {
 };
 
 const displayAllPlants = (plant) => {
-  // console.log(plant);
+  console.log(plant);
   plant.forEach((plnt) => {
     plantContainer.innerHTML += `
      
@@ -88,7 +87,7 @@ const displayAllPlants = (plant) => {
     <img
       src="${plnt.image}" />
   </figure>
-  <div class="card-body">
+  <div id="${plnt.id}" class="card-body">
     <h2 class="card-title font-bold">${plnt.name}</h2>
     <p class="font-semibold">${plnt.description}</p>
    <div class="flex justify-between items-center">
@@ -97,13 +96,96 @@ const displayAllPlants = (plant) => {
   </div>
   <div class="text-green-700 font-bold text-lg"><p>৳<span>${plnt.price}</span></p></div>
    </div>
-  <button id="cart-btn" class="btn bg-green-600 text-white w-full rounded-3xl mt-6">Add to Cart</button>
+  <button class="btn bg-green-600 text-white w-full rounded-3xl mt-6">Add to Cart</button>
 </div>
     `;
   });
 };
 
-showAllPlants();
+// plantContainer.addEventListener("click", (e) => {
+//   // console.log(e.target.parentNode.children[2].children[1].innerText);
+//   // console.log(e.target);
+//   if (e.target.innerText === "Add to Cart") {
+//     // console.log("add to cart cooked");
+//     handleCarts(e);
+
+//     //
+//   }
+// });
+// const handleCarts = (e) => {
+//   const paragraph = e.target.parentNode.children[0].innerText;
+//   // console.log(paragraph);
+//   const id = e.target.parentNode.id;
+//   // console.log(id);
+//   cart.push({
+//     paragraph: paragraph,
+//     id: id,
+//   });
+//   displayAllCarts(cart);
+// };
+
+// const displayAllCarts = (cart) => {
+//   cart.forEach((crt) => {
+//     cartContainer.innerHTML += `
+
+//   <div>
+//   <h1>${crt.name}</h1>
+//   <p>${crt.price}</p>
+//   </div>
+
+//   `;
+//   });
+// };
+
 plantContainer.addEventListener("click", (e) => {
-  console.log(e.target.parentNode.children[0]);
+  if (e.target.innerText === "Add to Cart") {
+    handleCarts(e);
+  }
 });
+
+const handleCarts = (crt) => {
+  // go up from button → card-body
+  const cardBody = crt.target.parentNode; // parent of the button = card-body\
+  // console.log(cardBody);
+
+  // extract data
+  const name = cardBody.children[0].innerText; // h2 is the first child
+  const price = cardBody.children[2].children[1].innerText;
+  const id = cardBody.id;
+
+  cart.push({
+    id: id,
+    name: name,
+    price: price,
+  });
+
+  displayAllCarts(cart);
+};
+
+const displayAllCarts = (cart) => {
+  cartContainer.innerHTML = "";
+  cart.forEach((crt) => {
+    cartContainer.innerHTML += `
+     
+      <div class="p-2 rounded-xl  mb-5 bg-[#f0fdf4] shadow-lg flex items-center justify-between">
+        <div>
+        <h1 class="font-bold">${crt.name}</h1>
+        <p class="text-green-600">৳${crt.price}</p>
+        </div>
+        <div onclick="handleDeleteCarts('${crt.id}')" class="cursor-pointer">
+        ❌
+        </div>
+      </div>
+    `;
+  });
+};
+
+const handleDeleteCarts = (cartsId) => {
+  // console.log(cartsId);
+  const filteredCarts = cart.filter((crt) => crt.id !== cartsId);
+  // console.log(filteredCarts);
+  cart = filteredCarts;
+  displayAllCarts(cart);
+};
+
+showAllPlants();
